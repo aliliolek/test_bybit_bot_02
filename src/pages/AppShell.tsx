46769f6f030e4e3b8682f9dcf96e7d6bfb8743d6
Oleksii Pishchuhin <51528776+aliliolek@users.noreply.github.com>
2025-08-09 23:45:41 +0200

    Add routing and placeholder pages (#6)

---
+++ src/pages/AppShell.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate, useLocation, useNavigate } from 'react-router-dom';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+
+import P2P from './P2P';
+import Orders from './Orders';
+import Ads from './Ads';
+import Profile from './Profile';
+
+const Navigation: React.FC = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const [value, setValue] = React.useState(location.pathname);
+
+  React.useEffect(() => {
+    setValue(location.pathname);
+  }, [location.pathname]);
+
+  const handleChange = (_event: React.SyntheticEvent, newValue: string) => {
+    setValue(newValue);
+    navigate(newValue);
+  };
+
+  return (
+    <BottomNavigation value={value} onChange={handleChange} showLabels>
+      <BottomNavigationAction label="P2P" value="/p2p" />
+      <BottomNavigationAction label="Orders" value="/orders" />
+      <BottomNavigationAction label="Ads" value="/ads" />
+      <BottomNavigationAction label="Profile" value="/profile" />
+    </BottomNavigation>
+  );
+};
+
+const AppShell: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/p2p" replace />} />
+        <Route path="/p2p" element={<P2P />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/ads" element={<Ads />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+      <Navigation />
+    </Router>
+  );
+};
+
+export default AppShell;
